be3c2c2b826ebddfd1ad1cf6b2f13f75f1f1783b
Stefaneco <67236866+Stefaneco@users.noreply.github.com>
2021-09-21 02:02:23 +0200

    Update disco.py

--- disco.py
+++ disco.py
@@ -2,8 +2,8 @@
 import json
 import requests
 
-webhook_gang = "https://discordapp.com/api/webhooks/596814508459294730/dR1PWAoZIKikJ3G7BDwL8LV7wTysmZwHFWBkp0z1I6kjuRRykYuc4ukXFUdjTJ4PVSpe"
-webhook_server = "https://discordapp.com/api/webhooks/597742331164098590/KMOSLZTEytHZ6Y0aOU0JTa3aBMWnjuCZqB3kD_DrQJpcZjgntCKkBdRpOI-1CNVZBHGd"
+webhook_gang = "https://discordapp.com/api/webhooks/XXXXXX"
+webhook_server = "https://discordapp.com/api/webhooks/XXXXXX"
 
 def sendWeebhook(webhook_url, message):
 	payload = {
